d224322954d78f4be470b55fc79b52acacd7a6d7
Jin Eun Jeong <wlsdms0122@gmail.com>
2020-07-28 22:40:08 +0900

    feat(): Initialize project

---
+++ rx_playground/Source/Module/Main/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  rx_playground
+//
+//  Created by JSilver on 2020/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
